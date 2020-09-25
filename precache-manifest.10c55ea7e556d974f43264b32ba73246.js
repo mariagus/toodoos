c9a27974484ce326ed7c4a3b41f49b435aacdc11
Maria Gusova <masha.gu@gmail.com>
2020-09-25 17:38:43 +0100

    Updates

---
+++ precache-manifest.10c55ea7e556d974f43264b32ba73246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fea564b797cb256c70732cf1aa970de",
+    "url": "/toodoos/index.html"
+  },
+  {
+    "revision": "f490e2f755fd43173fbf",
+    "url": "/toodoos/static/css/main.7302a748.chunk.css"
+  },
+  {
+    "revision": "d1447ee11199b841ef29",
+    "url": "/toodoos/static/js/2.4656ea7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toodoos/static/js/2.4656ea7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f490e2f755fd43173fbf",
+    "url": "/toodoos/static/js/main.d8164832.chunk.js"
+  },
+  {
+    "revision": "b0295ab3ef8521ddd301",
+    "url": "/toodoos/static/js/runtime-main.0f2a035e.js"
+  }
+]);
